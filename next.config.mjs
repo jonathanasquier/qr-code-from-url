6220327de02506d3b4710d8111be40071c207ae9
Jonathan Asquier <jonathan.asquier@gmail.com>
2024-05-25 16:56:57 +0200

    change build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "docs",
-  assetPrefix: "https://jonathanasquier.github.io/qr-code-from-url/",
+  // output: "export",
+  // distDir: "docs",
+  // assetPrefix: "https://jonathanasquier.github.io/qr-code-from-url/",
 };
 
 export default nextConfig;
